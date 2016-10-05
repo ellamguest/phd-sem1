ebcbc58dbfea2f878e302594955af79bd776af9c
ellamguest <ella.guest@postgrad.manchester.ac.uk>
2016-10-05 12:34:51 +0100

    creating images of basic nets

--- networkx_scratch.py
+++ networkx_scratch.py
@@ -8,6 +8,7 @@ Created on Tue Oct 04 17:18:28 2016
 import networkx as nx
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from pull_sub_info import mod_sub_matrix
 
 df = pd.read_csv('mod_by_sub_matrix.csv')
@@ -19,11 +20,20 @@ dfT = dfT.drop('index', axis=1)
 
 A = np.matrix(df)
 AT = np.matrix(dfT)
-M = np.dot(A,AT) # 57 x 57
+M = np.dot(A,AT) # 57 x 57 mods
+S = np.dot(AT,A) # 24 x 24 subs
+np.savetxt('subxsub.csv',S)
 
 G=nx.from_numpy_matrix(M)
+G2=nx.from_numpy_matrix(S)
 
-# look at metwrok attributes
+# look at network attributes
 nx.connected_components(G)
 sorted(nx.degree(G).values())
 nx.clustering(G)
+nx.clustering(G2)
+nx.degree(G,1)
+
+# drawing network
+nx.draw(G2)
+plt.savefig("basic_co-moderated_net.png")
