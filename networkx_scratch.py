e76ff1d6d8890fde61ea067810edd373843c64e6
ellamguest <ella.guest@postgrad.manchester.ac.uk>
2016-10-05 15:40:49 +0100

    testing networkx attribute functions

--- networkx_scratch.py
+++ networkx_scratch.py
@@ -22,12 +22,16 @@ A = np.matrix(df)
 AT = np.matrix(dfT)
 M = np.dot(A,AT) # 57 x 57 mods
 S = np.dot(AT,A) # 24 x 24 subs
+np.savetxt('modxmod.csv',M)
 np.savetxt('subxsub.csv',S)
+
+M = np.genfromtxt('modxmod.csv')
+S = np.genfromtxt('subxsub.csv')
 
 G=nx.from_numpy_matrix(M)
 G2=nx.from_numpy_matrix(S)
 
-# look at network attributes
+# look at node attributes
 nx.connected_components(G)
 sorted(nx.degree(G).values())
 nx.clustering(G)
@@ -37,3 +41,22 @@ nx.degree(G,1)
 # drawing network
 nx.draw(G2)
 plt.savefig("basic_co-moderated_net.png")
+
+# clustering coefficients:
+cc = sorted(nx.clustering(G).values())
+cc2 = sorted(nx.clustering(G2).values())
+
+
+plt.plot(cc)
+plt.ylabel('clustering coefficient')
+plt.show()
+
+# identify nodes by clustering coefficent
+ x = nx.clustering(G)
+for key,value in x.iteritems():
+    if value == 1.0:
+        print key
+
+# look at network attributes
+nx.node_connectivity(G) # approximation for node connectivity for whole graph
+nx.center(G)
