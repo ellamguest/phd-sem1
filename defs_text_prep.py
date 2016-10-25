82a4e6968495d2ced66b7c063c6d61dc1f270fd3
ellamguest <ellamguest@gmail.com>
2016-10-25 15:46:10 +0100

    attempting textblob for text analysis

--- defs_text_prep.py
+++ defs_text_prep.py
@@ -6,9 +6,31 @@ Created on Tue Oct 25 11:51:58 2016
 """
 
 import pandas as pd
+import string
+from textblob import TextBlob
 
 df = pd.read_csv('/Users/emg/Programmming/GitHub/phd-sem1/default_subs.csv', index_col=0)
-
 text = df[['display_name','submit_text', 'public_description']]
+text['public_description'] = text['public_description'].fillna('')
+text['submit_text'] = text['submit_text'].fillna('')
 
-text.to_csv('defs_text.csv')
+
+# own word parsing
+def get_words(text):
+    words = []
+    text = text.lower()
+    for x in text.split():
+        words.append(x.strip(string.punctuation))
+    return words
+    
+
+text['desc_words'] = text['public_description'].apply(get_words)
+text['submit_words'] = text['submit_text'].apply(get_words)
+'
+text.to_csv('defs_text.txt')
+
+# text analysis w/ textblob
+
+x = text.iloc[0]['public_description']
+blob = TextBlob(x)
+blob.tags
