29675e3e7a5fb88e3558131970a84b65e9430063
ellamguest <ella.guest@postgrad.manchester.ac.uk>
2017-08-29 17:58:37 +0100

    -

--- scratch.py
+++ scratch.py
@@ -6,4 +6,19 @@ Created on Tue Aug 29 11:57:29 2017
 """
 
 years = ['2015','2016','2017']
-months = ["%.2d" % i for i in range(13)]
+months = ["%.2d" % i for i in range(1, 13)]
+
+dates = []
+for year in years:
+    for month in months:
+        dates.append(year + '_' + month)
+        
+td_dates = dates[5:-5]
+
+datasets = []
+for date in td_dates:
+    datasets.append('[fh-bigquery:reddit_comments.{}]'.format(date))
+
+s = ''
+for d in datasets:
+    s += d + ', '
